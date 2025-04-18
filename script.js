23175eef225bafa63a1dc9d87a324da9b9867ef1
G MOUKTHIKA SRI SRUJANA <mgogula@gitam.in>
2025-04-18 18:10:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showEvent(eventName) {
+    alert("You clicked on: " + eventName);
+  }
+  
+  function notifyUser() {
+    alert("Thank you for your interest! We'll notify you about upcoming events.");
+  }
+  
